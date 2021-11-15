f808e1c6dbc4c78565947807f840bb769d162ad8
DamianPrzeslaw <damianprzeslaw@gmail.com>
2021-11-15 11:41:03 +0100

    new

--- Controllers/TokensController.cs
+++ Controllers/TokensController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Portfolio_Krypto.Models;
 
 namespace Portfolio_Krypto.Controllers
 {
+    [Authorize]
     public class TokensController : Controller
     {
         private readonly ApplicationDbContext _context;
